b3d08026e22cb80f60be2736f625f289f5cafb2d
Dillan444 <rojasdylan444@gmail.com>
2023-03-01 17:44:14 -0500

    Se agregaron comentarios al código

--- array.js
+++ array.js
@@ -8,10 +8,12 @@ class MyArray {
         this.data = {};
     }
 
+    // Obtiene un elemento en la posición(index) indicada
     get(index) {
         return this.data[index]
     }
 
+    // Incerta un nuevo elemento(item) en la posición del largo del array. Luego se incrementa en uno el largo
     push(item) {
         this.data[this.length] = item;
         this.length++;
@@ -19,7 +21,8 @@ class MyArray {
     }
 }
 
-const myArray = new MyArray();
-console.log(myArray.push("Luis"));
-console.log(myArray.push("Andres"));
-console.log(myArray.get(1));
+const myArray = new MyArray();       //Instancia de la clase(MyArray)
+console.log(myArray.push("Luis"));   //Insertar elemento luis
+console.log(myArray.push("Andres")); //Insertar elemento Andres
+console.log(myArray.get(1));         //Obtener el elemento en la posición 1
+console.log(myArray.length);
