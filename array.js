350c5da037f792d45cb47ab5b2a47de28993e9a0
Dillan444 <rojasdylan444@gmail.com>
2023-03-01 18:31:03 -0500

    Eliminando elementos del Array

--- array.js
+++ array.js
@@ -19,10 +19,38 @@ class MyArray {
         this.length++;
         return this.data;
     }
+
+    // Elimina el ultimo elemento de un array
+    pop(){
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    // Eliminar un elemento en el indice(index) indicado
+    delete(index) {
+        const item = this.data[index];
+        this.shiftIndex(index);
+
+        return item;
+    }
+
+    // Realiza el cambio de indice(index) de los elemento posteriores al elemento a eliminar
+    shiftIndex(index) {
+        for(let i  = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
 }
 
 const myArray = new MyArray();       //Instancia de la clase(MyArray)
 console.log(myArray.push("Luis"));   //Insertar elemento luis
 console.log(myArray.push("Andres")); //Insertar elemento Andres
+console.log(myArray.push("Jorge"));  //Insertar elemento Jorge
 console.log(myArray.get(1));         //Obtener el elemento en la posición 1
-console.log(myArray.length);
+console.log(myArray.pop());
+console.log(myArray.delete(1));
+console.log(myArray.data);
