77e9e9d475a8a3c565422522332bd4412193d7d0
Dillan444 <rojasdylan444@gmail.com>
2023-03-02 19:24:07 -0500

    Implementado el método Get

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -23,10 +23,28 @@ class HashTable {
         this.data[address].push([key, value]);
         return this.data;
     }
+
+    // Busca y retorna el dato buscado
+    get(key) {
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if(currentBucket) {
+            for(let i = 0; i < currentBucket.length; i++) {
+                if(currentBucket[i][0] === key){
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
+    }
 }
 
 const myHashTable = new HashTable(50);
 console.log(myHashTable);
 myHashTable.set('Diego', 2002);
 myHashTable.set('Maria', 2006);
-console.log(myHashTable.set('Samuel', 1995));
+myHashTable.set('Samuel', 1995);
+console.log(myHashTable.get('Diego'));
+console.log(myHashTable.get('Maria'));
+console.log(myHashTable.get('Samuel'));
+console.log(myHashTable.data);
