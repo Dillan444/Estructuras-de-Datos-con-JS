daacbadf63f7928ac2ffbd9a36a7b69c00ae4789
Dillan444 <rojasdylan444@gmail.com>
2023-03-03 21:11:57 -0500

    Implemanetación metodo remove

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -66,7 +66,7 @@ class MySinglyLinkedLinst {
         }
 
         const newNode = new Node(value);
-        const firstPointer = this.getTheIndex(index - 1);
+        const firstPointer = this.getTheIndex(index - 1, 0);
         const holdingPointer = firstPointer.next;
         firstPointer.next = newNode;
         newNode.next = holdingPointer;
@@ -76,9 +76,9 @@ class MySinglyLinkedLinst {
     }
 
     // Obtiene el elemeto de la posición(index) indicada
-    getTheIndex(index) {
-        let counter = 0;
+    getTheIndex(index, i) {
         let currentNode = this.head;
+        let counter = i;
 
         while(counter != index){
             currentNode = currentNode.next;
@@ -87,13 +87,49 @@ class MySinglyLinkedLinst {
 
         return currentNode;
     }
+
+    // Elimina un elemento en la posición(index) indicada
+    remove(index){
+        if(index == 1){
+            return this.removeHead();
+        }else if(index > this.length){
+            return undefined;
+        }
+        
+        const beforePointer = this.getTheIndex(index - 1, 1);
+        const removed = beforePointer.next.value;
+        let afterPointer;
+        
+        if(index == this.length){
+            beforePointer.next = null;
+            this.tail = beforePointer;
+            this.length--;
+            return removed;
+        }
+        
+        afterPointer = beforePointer.next.next
+        beforePointer.next = afterPointer;
+        this.length--;
+
+        return removed;
+    }
+
+    // Elimna el primer elemeto del Nodo.
+    removeHead(){
+        const removed = this.head.value;
+        const afterPointer = this.head.next;
+        this.head = afterPointer;
+        this.length--;
+        return removed;
+    }
 }
 
 let mySinglyLinkedLinst = new MySinglyLinkedLinst(1);
 mySinglyLinkedLinst.append(2);
 mySinglyLinkedLinst.append(3);
-mySinglyLinkedLinst.append(4);
-mySinglyLinkedLinst.append(6);
+mySinglyLinkedLinst.append('f');
+mySinglyLinkedLinst.append(10);
 mySinglyLinkedLinst.prepend(0);
-mySinglyLinkedLinst.insert(3, 5);
+console.log(mySinglyLinkedLinst.remove(99));
+mySinglyLinkedLinst.insert(6, 5);
 console.log(mySinglyLinkedLinst);
