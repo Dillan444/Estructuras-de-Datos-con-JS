56faed28da75b6d4f8738076e91ea31c94635546
Dillan444 <rojasdylan444@gmail.com>
2023-03-07 21:24:20 -0500

    BinarySearchTree: Metodo insert

---
+++ tree/tree.js
@@ -0,0 +1,53 @@
+class Node{
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BinatySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // Inserta un nuevo valor en un nodo
+    insert(value) {
+        const newNode = new Node(value);
+
+        if(!this.root) {
+            this.root = newNode;
+        }else {
+            let currentNode = this.root;
+            while(true) {
+                if(value < currentNode.value) { //Izquierda
+                    if(!currentNode.left){
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left
+
+                }else { // Derecha
+                    if(!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+
+}
+
+const myTree = new BinatySearchTree();
+
+myTree.insert(10);
+  myTree.insert(4);
+    myTree.insert(2);
+    myTree.insert(8);
+  myTree.insert(20);
+    myTree.insert(17);
+    myTree.insert(20);
+
+console.log(myTree);
